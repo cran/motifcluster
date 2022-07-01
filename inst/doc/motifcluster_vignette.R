8600babbae6ea0a488e75cce2bacd6b13d5817eb
William George Underwood <wgu2@princeton.edu>
2022-07-01 20:30:10 +0000

    version 0.2.1

--- inst/doc/motifcluster_vignette.R
+++ inst/doc/motifcluster_vignette.R
@@ -4,9 +4,10 @@ knitr::opts_chunk$set(
   comment = "#> "
 )
 set.seed(200401293)
-old_options = options()
+old_options <- options()
 options(digits = 2)
 library(mclust)
+# nolint start: line_length_linter
 
 ## ---- eval = FALSE------------------------------------------------------------
 #  library(devtools)
@@ -49,8 +50,8 @@ mam_dense  <- build_motif_adjacency_matrix(G1, motif_name = "M1", mam_method = "
 all(mam_sparse == mam_dense)
 
 ## -----------------------------------------------------------------------------
-block_sizes = c(5, 3)
-connection_matrix = matrix(c(
+block_sizes <- c(5, 3)
+connection_matrix <- matrix(c(
   0.9, 0.2,
   0.2, 0.9
 ), nrow = 2, byrow = TRUE)
@@ -59,7 +60,7 @@ connection_matrix = matrix(c(
 sample_dsbm(block_sizes, connection_matrix)
 
 ## -----------------------------------------------------------------------------
-weight_matrix = matrix(c(
+weight_matrix <- matrix(c(
   5, 2,
   2, 5
 ), nrow = 2, byrow = TRUE)
@@ -67,19 +68,19 @@ sample_dsbm(block_sizes, connection_matrix, weight_matrix,
   sample_weight_type = "constant")
 
 ## -----------------------------------------------------------------------------
-sample_dsbm(block_sizes, connection_matrix, weight_matrix, sample_weight_type = "poisson")
+sample_dsbm(block_sizes, connection_matrix, weight_matrix,
+  sample_weight_type = "poisson")
 
 ## -----------------------------------------------------------------------------
-source_block_sizes = c(2)
-destination_block_sizes = c(3, 2)
-bipartite_connection_matrix = matrix(c(0.9, 0.2), nrow = 1)
+source_block_sizes <- c(2)
+destination_block_sizes <- c(3, 2)
+bipartite_connection_matrix <- matrix(c(0.9, 0.2), nrow = 1)
 sample_bsbm(source_block_sizes, destination_block_sizes, bipartite_connection_matrix)
 
 ## -----------------------------------------------------------------------------
-bipartite_weight_matrix = matrix(c(7, 2), nrow = 1)
-sample_bsbm(source_block_sizes, destination_block_sizes,
-  bipartite_connection_matrix, bipartite_weight_matrix,
-  sample_weight_type = "poisson")
+bipartite_weight_matrix <- matrix(c(7, 2), nrow = 1)
+sample_bsbm(source_block_sizes, destination_block_sizes, bipartite_connection_matrix,
+  bipartite_weight_matrix, sample_weight_type = "poisson")
 
 ## -----------------------------------------------------------------------------
 G2 <- matrix(c(
@@ -96,7 +97,7 @@ build_laplacian(G2, type_lap = "comb")
 build_laplacian(G2, type_lap = "rw")
 
 ## -----------------------------------------------------------------------------
-spectrum = run_laplace_embedding(G2, num_eigs = 2, type_lap = "rw")
+spectrum <- run_laplace_embedding(G2, num_eigs = 2, type_lap = "rw")
 spectrum$vals
 spectrum$vects
 
@@ -109,42 +110,43 @@ G3 <- matrix(c(
 ), byrow = TRUE, nrow = 4)
 
 ## -----------------------------------------------------------------------------
-spectrum = run_motif_embedding(G3, motif_name = "M1", motif_type = "func",
+spectrum <- run_motif_embedding(G3, motif_name = "M1", motif_type = "func",
   mam_weight_type = "unweighted", mam_method = "sparse", num_eigs = 2,
   restrict = TRUE, type_lap = "rw")
 spectrum$vals
 spectrum$vects
 
 ## -----------------------------------------------------------------------------
-block_sizes = rep(10, 3)
+block_sizes <- rep(10, 3)
 
 ## -----------------------------------------------------------------------------
-connection_matrix = matrix(c(
+connection_matrix <- matrix(c(
   0.8, 0.2, 0.2,
   0.2, 0.8, 0.2,
   0.2, 0.2, 0.8
 ), nrow = 3)
 
 ## -----------------------------------------------------------------------------
-weight_matrix = matrix(c(
+weight_matrix <- matrix(c(
   20, 10, 10,
   10, 20, 10,
   10, 10, 20
 ), nrow = 3)
 
-G4 = sample_dsbm(block_sizes, connection_matrix, weight_matrix,
+G4 <- sample_dsbm(block_sizes, connection_matrix, weight_matrix,
   sample_weight_type = "poisson")
 
 ## -----------------------------------------------------------------------------
-motif_cluster = run_motif_clustering(G4, motif_name = "M1", motif_type = "func",
-  mam_weight_type = "mean", mam_method = "sparse", type_lap = "rw", num_eigs = 4,
-  num_clusts = 3
+motif_cluster <- run_motif_clustering(G4, motif_name = "M1", motif_type = "func",
+  mam_weight_type = "mean", mam_method = "sparse", type_lap = "rw",
+  num_eigs = 4, num_clusts = 3
 )
 
 ## -----------------------------------------------------------------------------
-truth = c(rep(1, 10), rep(2, 10), rep(3, 10))
+truth <- c(rep(1, 10), rep(2, 10), rep(3, 10))
 mclust::adjustedRandIndex(motif_cluster$clusts, truth)
 
 ## ---- include = FALSE---------------------------------------------------------
 options(old_options)
+#nolint end
 
